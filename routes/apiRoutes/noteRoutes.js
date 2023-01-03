c1caebaa68631c523f5c8429cb60592e4509e7d3
Suzanne Banasihan <sjb04@ymail.com>
2023-01-02 23:57:15 -0800

    fixed routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,28 +1,25 @@
 const router = require("express").Router();
+const {noteCreateNewNote,noteDeleteNote} = require('../../lib/note');
+let {notesArray} = require('../../Develop/db/db');
 
-const {
-    notes
-} = require('../../Develop/db/db');
 
-const {
-    noteCreateNewNote,
-    noteDeleteNote
-} = require('../../lib/note');
+router.get('../../Develop/db/db', (req, res) => {
+    let results = notesArray;
+    res.json(results);
+});
 
-router.get('/notes', (req, res) => {
-    let saved = notes;
-    res.json(saved);
-})
+router.post('../../Develop/db/db', (req, res) => {
+    if(notesArray){
+    req.body.id = notesArray.length.toString();
+    } else 
+    {req.body.id = 0}
+    res.json(noteCreateNewNote(req.body, notesArray));
+});
 
-router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    let note = noteCreateNewNote(req.body, notes);
-    res.json(note);
-})
-
-router.delete('/notes/:id', (req, res) => {
-    noteDeleteNote(notes, req.params.id);
-    res.json(notes);
- })
+router.delete('/routes/apiRoutes/noteRoutes/:id', async function (req, res) {
+        const { id } = req.params;
+        notesArray = noteDeleteNote(notesArray, id);
+        res.json(notesArray);
+    });
 
  module.exports = router;
